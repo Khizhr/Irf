b241fcc48236d5cf2cff8eb5c7ea7c9953fb09ca
Khizhr <130645707+Khizhr@users.noreply.github.com>
2023-04-14 16:16:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var scrollButton = document.getElementById("abt");
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("int");
+element.scrollIntoView({behavior: "smooth"});
+});
+var scrollButton = document.getElementById("hob");
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("ftb");
+element.scrollIntoView({behavior: "smooth"});
+});
+var scrollButton = document.getElementById("skl");
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("set");
+element.scrollIntoView({behavior: "smooth"});
+});
